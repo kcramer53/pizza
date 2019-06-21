8b565ae56ba2d4374988739c03edd31076c3401f
Kirsten Putman <kirstenputman@Kirstens-MacBook-Pro.local>
2019-06-21 15:03:39 -0700

    added more functionality and js

--- README.md
+++ README.md
@@ -16,7 +16,13 @@ _{This is a detailed description of your application. Its purpose and usage.  Gi
 * _easy-to-understand_
 * _format_
 
-_{Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. Do I need to run a server? How should I set up my databases? Is there other code this app depends on?}_
+## Specs
+| Behavior | Input | Output |
+| ------------- |:-------------:| -----:|
+| b1 | i1 | o1 |
+| b2 | i2 | o2 |
+| b3 | i3 | o3 |
+| b4 | i4 | o4 |
 
 ## Known Bugs
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,38 @@
+// Business Logic //
+
+// naming function to call upon later //
+function Pizza(size, toppings, price){
+  this.size = size;
+  this.toppings = toppings;
+  this.price = price;
+};
+function Order(){
+  this.pizzaToppings = toppingsValue;
+  this.pizzaSize = size;
+  this.pizzaPrice = price
+};
+  Pizza.prototype.addPeperoni = function(){
+    return this.peperoni = true;
+  }
+  Pizza.prototype.addMushrooms = function(){
+    return this.mushrooms = true;
+  }
+  Pizza.prototype.addSausage = function(){
+  return this.addSausage = true;
+  }
+  Pizza.prototype.addPineapple = function(){
+  return this.addPineapple = true;
+  }
+
+
+// User Interface Logic //
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
+
+
+    var size = $("#sizSelect option:selected").text();
+    var toppings = $("#toppingsSelect option:selected").radio();
+
+    var newPizza = new Pizza(size, toppings, price);
+    var totalPrice = sizeValue + toppingsValue;
